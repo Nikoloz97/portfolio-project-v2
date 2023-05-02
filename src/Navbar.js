1c34fbb981d1ccc29d498ca7bfddb3fa6a3214b6
NickGotsy <NGotsiridze@accumtech.com>
2023-05-02 16:01:47 -0400

    Modified nav button css

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,18 +1,37 @@
 import React from "react";
-import { Menu, MenuItem } from "semantic-ui-react";
+import { Menu, MenuItem, Button } from "semantic-ui-react";
+import "./App.css";
 
 const Navbar = () => {
+  const navLinksStyle = {
+    color: "black",
+    padding: "10px",
+    textDecoration: "none",
+  };
+
   return (
     <div>
       <Menu className="Navbar">
         <MenuItem>
-          <a href={`/`}>Home</a>
+          <Button primary>
+            <a style={navLinksStyle} href={`/`}>
+              Home
+            </a>
+          </Button>
         </MenuItem>
         <MenuItem>
-          <a href={`/about`}>About</a>
+          <Button>
+            <a style={navLinksStyle} href={`/about`}>
+              About
+            </a>
+          </Button>
         </MenuItem>
         <MenuItem>
-          <a href={`/contact`}>Contact</a>
+          <button>
+            <a style={navLinksStyle} href={`/contact`}>
+              Contact
+            </a>
+          </button>
         </MenuItem>
       </Menu>
     </div>
