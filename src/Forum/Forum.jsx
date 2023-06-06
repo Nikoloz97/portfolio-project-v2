83155643dd5b4b6e61cfa988d3e7cdec1369889f
ATEC\NGotsiridze <NGotsiridze@AccumTech.com>
2023-06-05 22:35:56 -0400

    Mapped fetch data. Removed hard coded cards

--- src/Forum/Forum.jsx
+++ src/Forum/Forum.jsx
@@ -13,83 +13,38 @@ function Forum() {
 
   return (
     <div className="Forum-Page">
-      <Button onClick={getProfileCards}>Get ProfileCards</Button>
+      <Button onClick={getProfileCards}>Get Profile Cards</Button>
 
       <Card.Group>
-        <Card>
-          <Card.Content>
-            <Image
-              floated="right"
-              size="mini"
-              src="https://react.semantic-ui.com/images/avatar/large/steve.jpg"
-            />
-            <Card.Header>Steve Sanders</Card.Header>
-            <Card.Meta>Friends of Elliot</Card.Meta>
-            <Card.Description>
-              Steve wants to add you to the group <strong>best friends</strong>
-            </Card.Description>
-          </Card.Content>
-          <Card.Content extra>
-            <div className="ui two buttons">
-              <Button basic color="green">
-                Approve
-              </Button>
-              <Button basic color="red">
-                Decline
-              </Button>
-            </div>
-          </Card.Content>
-        </Card>
-
-        <Card>
-          <Card.Content>
-            <Image
-              floated="right"
-              size="mini"
-              src="https://react.semantic-ui.com/images/avatar/large/molly.png"
-            />
-            <Card.Header>Molly Thomas</Card.Header>
-            <Card.Meta>New User</Card.Meta>
-            <Card.Description>
-              Molly wants to add you to the group <strong>musicians</strong>
-            </Card.Description>
-          </Card.Content>
-          <Card.Content extra>
-            <div className="ui two buttons">
-              <Button basic color="green">
-                Approve
-              </Button>
-              <Button basic color="red">
-                Decline
-              </Button>
-            </div>
-          </Card.Content>
-        </Card>
-
-        <Card>
-          <Card.Content>
-            <Image
-              floated="right"
-              size="mini"
-              src="https://react.semantic-ui.com/images/avatar/large/jenny.jpg"
-            />
-            <Card.Header>Jenny Lawrence</Card.Header>
-            <Card.Meta>New User</Card.Meta>
-            <Card.Description>
-              Jenny requested permission to view your contact details
-            </Card.Description>
-          </Card.Content>
-          <Card.Content extra>
-            <div className="ui two buttons">
-              <Button basic color="green">
-                Approve
-              </Button>
-              <Button basic color="red">
-                Decline
-              </Button>
-            </div>
-          </Card.Content>
-        </Card>
+        {data.map((user) => (
+          <Card>
+            <Card.Content>
+              <Image
+                floated="right"
+                size="mini"
+                src="https://react.semantic-ui.com/images/avatar/large/steve.jpg"
+              />
+              <Card.Header>
+                {user.firstName} {user.lastName}
+              </Card.Header>
+              <Card.Meta>Friends of Elliot</Card.Meta>
+              <Card.Description>
+                Steve wants to add you to the group{" "}
+                <strong>best friends</strong>
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <div className="ui two buttons">
+                <Button basic color="green">
+                  Approve
+                </Button>
+                <Button basic color="red">
+                  Decline
+                </Button>
+              </div>
+            </Card.Content>
+          </Card>
+        ))}
       </Card.Group>
     </div>
   );
