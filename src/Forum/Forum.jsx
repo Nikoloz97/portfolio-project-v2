0716740c6299dfad635a2f12c2a3543f0b63fbe1
ATEC\NGotsiridze <NGotsiridze@AccumTech.com>
2023-06-07 09:00:13 -0400

    Fetch PC request on render

--- src/Forum/Forum.jsx
+++ src/Forum/Forum.jsx
@@ -1,38 +1,37 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import { Button, Card, Image } from "semantic-ui-react";
 import axios from "axios";
 
 function Forum() {
   const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getProfileCards();
+  }, []);
+
   const getProfileCards = () => {
     axios.get("https://localhost:7047/api/forum").then((response) => {
       setData(response.data);
     });
-    console.log(data);
   };
 
   return (
     <div className="Forum-Page">
-      <Button onClick={getProfileCards}>Get Profile Cards</Button>
-
       <Card.Group>
         {data.map((user) => (
           <Card>
             <Card.Content>
-              <Image
-                floated="right"
-                size="mini"
-                src="https://react.semantic-ui.com/images/avatar/large/steve.jpg"
-              />
+              <Image floated="right" size="mini" src={user.profileURL} />
               <Card.Header>
                 {user.firstName} {user.lastName}
               </Card.Header>
-              <Card.Meta>Friends of Elliot</Card.Meta>
-              <Card.Description>
-                Steve wants to add you to the group{" "}
-                <strong>best friends</strong>
-              </Card.Description>
+              <Card.Meta>{user.age} years old</Card.Meta>
+
+              {user.interests.map((interest) => (
+                <Card.Description>{interest.description}</Card.Description>
+              ))}
             </Card.Content>
+
             <Card.Content extra>
               <div className="ui two buttons">
                 <Button basic color="green">
