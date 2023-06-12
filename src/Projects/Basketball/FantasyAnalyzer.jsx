290f877814ad4650498f9d5228a1b0beed05b66d
ATEC\NGotsiridze <NGotsiridze@AccumTech.com>
2023-06-12 09:50:41 -0400

    Created test connection to bball api

--- src/Projects/Basketball/FantasyAnalyzer.jsx
+++ src/Projects/Basketball/FantasyAnalyzer.jsx
@@ -1,7 +1,36 @@
 import React, { useState } from "react";
-import { Form } from "semantic-ui-react";
+import {
+  Form,
+  Button,
+  Modal,
+  List,
+  Divider,
+  Segment,
+  Grid,
+  Header,
+} from "semantic-ui-react";
+
+import axios from "axios";
 
 function FantasyAnalyzer() {
+  const seasons = {
+    method: "GET",
+    url: "https://api-nba-v1.p.rapidapi.com/seasons",
+    headers: {
+      "X-RapidAPI-Key": "2a68275cbamsha4332cacc57200cp19e3b9jsn35ead262c58d",
+      "X-RapidAPI-Host": "api-nba-v1.p.rapidapi.com",
+    },
+  };
+
+  async function fetchSeasons() {
+    try {
+      const response = await axios.request(seasons);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   const [playerOptions, setPlayerOptions] = useState([]);
   const [selectedPlayers, setSelectedPlayers] = useState([
     {
@@ -70,10 +99,6 @@ function FantasyAnalyzer() {
     updatedSelectedPlayers[index].playerName = value;
     updatedSelectedPlayers[index].isModified = true;
     setSelectedPlayers(updatedSelectedPlayers);
-
-    console.log(index);
-    console.log(event.target.innerText);
-    console.log(selectedPlayers);
   };
 
   const playerDropdowns = [
@@ -138,6 +163,87 @@ function FantasyAnalyzer() {
       options: dummyPlayerOptions,
     },
   ];
+
+  //Modal stuff... (own component?)
+  const [open, setOpen] = useState(false);
+  const [userCats, setUserCats] = useState([
+    {
+      category: "PTS",
+      value: 16.5,
+    },
+    {
+      category: "FG%",
+      value: 48.5,
+    },
+    {
+      category: "FT%",
+      value: 78,
+    },
+    {
+      category: "3PM",
+      value: 2.5,
+    },
+    {
+      category: "REB",
+      value: 10,
+    },
+    {
+      category: "AST",
+      value: 6,
+    },
+    {
+      category: "STL",
+      value: 1.2,
+    },
+    {
+      category: "BLK",
+      value: 1.4,
+    },
+    {
+      category: "TO",
+      value: 2.2,
+    },
+  ]);
+
+  const [topCats, setTopCats] = useState([
+    {
+      category: "PTS",
+      value: 16.5,
+    },
+    {
+      category: "FG%",
+      value: 48.5,
+    },
+    {
+      category: "FT%",
+      value: 78,
+    },
+    {
+      category: "3PM",
+      value: 2.5,
+    },
+    {
+      category: "REB",
+      value: 10,
+    },
+    {
+      category: "AST",
+      value: 6,
+    },
+    {
+      category: "STL",
+      value: 1.2,
+    },
+    {
+      category: "BLK",
+      value: 1.4,
+    },
+    {
+      category: "TO",
+      value: 2.2,
+    },
+  ]);
+
   return (
     <div className="Default-Page">
       <header>Welcome to the fantasy analyzer</header>
@@ -157,6 +263,63 @@ function FantasyAnalyzer() {
         ))}
         <Form.Button>Submit</Form.Button>
       </Form>
+
+      <Button onClick={fetchSeasons}>Fetch Seasons Test</Button>
+
+      <Modal
+        onClose={() => setOpen(false)}
+        onOpen={() => setOpen(true)}
+        open={open}
+        trigger={<Button>Show Results</Button>}
+      >
+        <Modal.Header>Results</Modal.Header>
+
+        <Modal.Content>
+          <Segment>
+            <Grid columns={2} relaxed="very">
+              <Grid.Column>
+                <Header>Your team</Header>
+
+                <List selection verticalAlign="middle">
+                  {userCats.map((x) => (
+                    <List.Item>
+                      <List.Content>
+                        <List.Header>{x.category}</List.Header>
+                        <List>{x.value}</List>
+                      </List.Content>
+                    </List.Item>
+                  ))}
+                </List>
+              </Grid.Column>
+              <Grid.Column>
+                <Header>Top-150 Average</Header>
+                <List selection verticalAlign="middle">
+                  {topCats.map((x) => (
+                    <List.Item>
+                      <List.Content>
+                        <List.Header>{x.category}</List.Header>
+                        <List>{x.value}</List>
+                      </List.Content>
+                    </List.Item>
+                  ))}
+                </List>
+              </Grid.Column>
+            </Grid>
+
+            <Divider vertical>VS</Divider>
+          </Segment>
+        </Modal.Content>
+
+        <Modal.Actions>
+          <Button
+            content="Return"
+            labelPosition="left"
+            icon="checkmark"
+            onClick={() => setOpen(false)}
+            positive
+          />
+        </Modal.Actions>
+      </Modal>
     </div>
   );
 }
--- src/User/Login.jsx
+++ src/User/Login.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "semantic-ui-react";
+import { Button, Divider, Grid, Segment, Form } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
 const Login = () => {
@@ -7,10 +7,40 @@ const Login = () => {
     <>
       <header className="Default-Page">
         <p>Welcome to the login page</p>
-        <p>New user?</p>
-        <Button as={Link} to="/signUp">
-          Sign up
-        </Button>
+        <Segment placeholder>
+          <Grid columns={2} relaxed="very" stackable>
+            <Grid.Column>
+              <Form>
+                <Form.Input
+                  icon="user"
+                  iconPosition="left"
+                  label="Username"
+                  placeholder="Username"
+                />
+                <Form.Input
+                  icon="lock"
+                  iconPosition="left"
+                  label="Password"
+                  type="password"
+                />
+
+                <Button content="Login" primary />
+              </Form>
+            </Grid.Column>
+
+            <Grid.Column verticalAlign="middle">
+              <Button
+                content="Sign up"
+                icon="signup"
+                size="big"
+                as={Link}
+                to="/signUp"
+              />
+            </Grid.Column>
+          </Grid>
+
+          <Divider vertical>Or</Divider>
+        </Segment>
       </header>
     </>
   );
