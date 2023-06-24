1b3429dedbd411b9597586bfbb609ece267535a1
ATEC\NGotsiridze <NGotsiridze@AccumTech.com>
2023-06-24 16:40:14 -0400

    Updated contact form

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,12 +1,36 @@
 import React from "react";
+import { Button, Form } from "semantic-ui-react";
 import "../App.css";
 
 const Contact = () => {
+  const categoryOptions = [
+    { key: 1, text: "Job Offer", value: "jobOffer" },
+    { key: 2, text: "Project Collaboration", value: "projCollab" },
+    { key: 3, text: "Site issue", value: "siteIssue" },
+    { key: 4, text: "Other", value: "other" },
+  ];
+
   return (
-    <div>
-      <header className="Default-Page">
-        <p>Welcome to the contact page</p>
-      </header>
+    <div className="Default-Page">
+      <header>Welcome to the contact page</header>
+      <Form className="Default-Form" style={{ width: "40%" }}>
+        <Form.Group widths="equal" style={{ marginTop: "10px" }}>
+          <Form.Input fluid label="First name" placeholder="First name" />
+          <Form.Input fluid label="Last name" placeholder="Last name" />
+        </Form.Group>
+
+        <Form.Group widths="equal">
+          <Form.Input fluid label="Email" placeholder="Your email" />
+          <Form.Select fluid label="Category" options={categoryOptions} />
+        </Form.Group>
+
+        <Form.TextArea
+          label="Description"
+          placeholder="A few sentences would be appreciated..."
+        />
+
+        <Form.Button>Submit</Form.Button>
+      </Form>
     </div>
   );
 };
