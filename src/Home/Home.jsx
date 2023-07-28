98f6394ead37b5c9c30e19aa555abdc6d2ec15fa
Nikoloz97 <nick.gotsy@gmail.com>
2023-07-28 11:04:16 -0400

    Test 4

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -7,6 +7,7 @@ const Home = () => {
     <>
       <header className="Default-Page">
         <p>Welcome to my porfolio page</p>
+        <p>Test 4</p>
         <div>
           <Button as={Link} to="/login">
             Log in
