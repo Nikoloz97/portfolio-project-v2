1eea35a00f199885eccc91d6a4a2676d4c0dc593
Nikoloz97 <nick.gotsy@gmail.com>
2023-09-12 10:05:17 -0400

    Home - arrow fades in after buttons

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,21 +1,24 @@
 import React, { useState, useEffect } from "react";
-import { Button, Header } from "semantic-ui-react";
+import { Button, Header, Grid, Icon } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import { useUserContext } from "../UserContext";
 import "./Home.css";
 
 const Home = () => {
   const { user, setUser, setIsUserSignedIn } = useUserContext();
-  const [showButtons, setShowButtons] = useState(false);
+
   const [text, setText] = useState("");
   const [isTyping, setIsTyping] = useState(true);
+
+  const [showButtons, setShowButtons] = useState(false);
+  const [showArrow, setShowArrow] = useState(false);
 
   const handleSignOut = () => {
     setUser(null);
     setIsUserSignedIn(false);
   };
 
-  // Welcome animation
+  // Welcome - typewriting animation
   useEffect(() => {
     const welcomeText = "Welcome.";
 
@@ -25,10 +28,13 @@ const Home = () => {
         setText(welcomeText.substring(0, currentIndex));
         currentIndex++;
       } else {
-        clearInterval(typingInterval);
         setIsTyping(false);
         setShowButtons(true);
       }
+
+      setTimeout(() => {
+        setShowArrow(true);
+      }, 1500);
     }, 100); // Typing speed (100ms per character)
     return () => {
       clearInterval(typingInterval);
@@ -49,32 +55,56 @@ const Home = () => {
   return (
     <>
       <header className="Default-Page">
-        <Header className="typewriter-animation" style={{ color: "white" }}>
-          {isTyping ? (
-            <span className="typewriter-animation">{text}</span>
-          ) : (
-            text
-          )}
-        </Header>
+        <div className="welcome-buttons-container">
+          <Grid verticalAlign="middle" centered>
+            <Grid.Row>
+              <Header
+                className="typewriter-animation"
+                style={{ color: "white" }}
+              >
+                {isTyping ? (
+                  <span className="typewriter-animation">{text}</span>
+                ) : (
+                  text
+                )}
+              </Header>
+            </Grid.Row>
+            <Grid.Row>
+              {showButtons ? (
+                <div className="button-container">
+                  {user === undefined || user === null ? (
+                    <div>
+                      <Button as={Link} to="/login">
+                        Log In
+                      </Button>
+                      <Button as={Link} to="/signup">
+                        Sign Up
+                      </Button>
+                    </div>
+                  ) : (
+                    <div>
+                      <Button onClick={handleSignOut}>Sign Out</Button>
+                    </div>
+                  )}
+                </div>
+              ) : null}
+            </Grid.Row>
 
-        {showButtons ? (
-          <div className="button-container">
-            {user === undefined || user === null ? (
-              <div>
-                <Button as={Link} to="/login" className="fade-in-button">
-                  Log In
-                </Button>
-                <Button as={Link} to="/signup" className="fade-in-button">
-                  Sign Up
-                </Button>
-              </div>
-            ) : (
-              <div>
-                <Button onClick={handleSignOut}>Sign Out</Button>
-              </div>
-            )}
-          </div>
-        ) : null}
+            {showArrow ? (
+              <Grid.Row>
+                <div className="arrow-container">
+                  <Icon
+                    name="arrow down"
+                    size="small"
+                    onClick={() => {
+                      // Handle arrow click event
+                    }}
+                  />
+                </div>
+              </Grid.Row>
+            ) : null}
+          </Grid>
+        </div>
       </header>
     </>
   );
