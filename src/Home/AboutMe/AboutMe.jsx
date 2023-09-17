87e9bf1281488a4a3efa6021e81c394a8c720efc
Nikoloz97 <nick.gotsy@gmail.com>
2023-09-17 06:21:03 -0400

    Home - attempted to scale down arrow

---
+++ src/Home/AboutMe/AboutMe.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AboutMe = (props) => {
+  return (
+    <div className="Default-Page" style={{ height: "100vh" }}>
+      Welcome to about me
+    </div>
+  );
+};
+
+export default AboutMe;
