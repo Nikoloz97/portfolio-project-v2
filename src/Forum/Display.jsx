2189926844e01ea4762129ad757a65fff1778bf1
Nikoloz97 <nick.gotsy@gmail.com>
2023-10-17 23:21:27 -0400

    Forum - loading screen while display image loading

--- src/Forum/Display.jsx
+++ src/Forum/Display.jsx
@@ -1,10 +1,12 @@
-import { React, useRef } from "react";
-import { Image, Button } from "semantic-ui-react";
+import { React, useRef, useState, useEffect } from "react";
+import { Image, Button, Loader } from "semantic-ui-react";
 import "./Forum.css";
 import Forum from "./Forum";
 
 function Display() {
   const forumRef = useRef(null);
+
+  const [loading, setLoading] = useState(true);
 
   const handleJoinForum = () => {
     forumRef.current.scrollIntoView({ behavior: "smooth" });
@@ -13,20 +15,35 @@ function Display() {
   return (
     <>
       <div>
-        <Image
-          src={require("../Images/ForumDisplay/Travel5.jpg")}
-          style={{ height: "100vh", width: "100%" }}
-          centered
-        />
-        <div className="forum-display-text">Welcome to the Forum</div>
-        <div className="forum-display-subtext">Express and Discuss</div>
-        <Button onClick={handleJoinForum} className="forum-display-button">
-          Join
-        </Button>
-      </div>
+        <div className="Default-Page">
+          <Image
+            src={require("../Images/ForumDisplay/Travel5.jpg")}
+            style={{ width: "100%" }}
+            centered
+            hidden={loading}
+            onLoad={() => setLoading(false)}
+          />
+          {loading ? (
+            <Loader active content="Loading" />
+          ) : (
+            <div>
+              <div className="forum-display-text">Welcome to the Forum</div>
+              <div className="forum-display-subtext">Express and Discuss</div>
+              <Button
+                onClick={handleJoinForum}
+                className="forum-display-button"
+              >
+                Join
+              </Button>
+            </div>
+          )}
+        </div>
 
-      <div ref={forumRef}>
-        <Forum />
+        {loading ? null : (
+          <div ref={forumRef}>
+            <Forum />
+          </div>
+        )}
       </div>
     </>
   );
