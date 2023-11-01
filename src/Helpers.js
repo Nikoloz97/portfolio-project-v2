a2b31157b74c54bf539dcb898365f991079cbdef
Nikoloz97 <nick.gotsy@gmail.com>
2023-10-31 21:03:01 -0400

    Helpers - converted all api controller routes

--- src/Helpers.js
+++ src/Helpers.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-export const apiUserRoot = "https://localhost:7047/api/user";
+export const apiUserRoot = "https://nicksportfolio.azurewebsites.net/api/user";
 
 export const apiForumRoot =
   "https://nicksportfolio.azurewebsites.net/api/forum";
 
-export const apiGeoGameRoot = "https://localhost:7047/api/geoGame";
+export const apiGeoGameRoot =
+  "https://nicksportfolio.azurewebsites.net/api/geoGame";
 
 export const apiContactRoot =
   "https://nicksportfolio.azurewebsites.net/api/contact";
