bb4cb46b8ccd31560b745fe921830c1a342659a6
Nikoloz97 <nick.gotsy@gmail.com>
2023-11-12 11:48:37 -0500

    Forum - created error modal screen

--- src/Forum/Display.jsx
+++ src/Forum/Display.jsx
@@ -1,5 +1,5 @@
 import { React, useEffect, useRef, useState } from "react";
-import { Image, Button, Loader } from "semantic-ui-react";
+import { Image, Button, Loader, Modal, Header } from "semantic-ui-react";
 import "./Forum.css";
 import Forum from "./Forum";
 
@@ -8,7 +8,9 @@ function Display() {
 
   const [isLoading, setIsLoading] = useState(true);
   const [isDisplayVisible, setIsDisplayVisible] = useState(false);
-  const [isRetryingFetch, setIsRetryingFetch] = useState(false);
+  const [isFetchSuccessful, setIsFetchSuccessful] = useState(null);
+  const [isErrorModalDisplayed, setIsErrorModalDisplayed] = useState(false);
+  // const [isRetryingFetch, setIsRetryingFetch] = useState(false);
 
   const handleScrollDown = () => {
     forumRef.current.scrollIntoView({ behavior: "smooth" });
@@ -21,6 +23,14 @@ function Display() {
       }, 200);
     }
   }, [isLoading]);
+
+  useEffect(() => {
+    if (isFetchSuccessful === false) {
+      setTimeout(() => {
+        setIsErrorModalDisplayed(true);
+      }, 200);
+    }
+  }, [isFetchSuccessful]);
 
   return (
     <>
@@ -35,7 +45,9 @@ function Display() {
             hidden={isLoading}
             className={`fade-in-display ${isDisplayVisible ? "fade-in" : ""}`}
           />
-          <div className={isRetryingFetch ? "Hide-Display" : ""}>
+          <div
+          // className={isRetryingFetch ? "Hide-Display" : ""}
+          >
             <div
               className={`fade-in-display ${isDisplayVisible ? "fade-in" : ""}`}
             >
@@ -44,18 +56,61 @@ function Display() {
               <Button
                 onClick={handleScrollDown}
                 className="forum-display-button"
+                disabled={isFetchSuccessful === false}
               >
                 Join
               </Button>
             </div>
+
+            {/* TODO: Why isn't error modal being displayed?? */}
+            {isErrorModalDisplayed === true ? (
+              <Modal
+              // onClose={() => setOpen(false)}
+              // onOpen={() => setOpen(true)}
+              // open={}
+              >
+                <Modal.Header>Select a Photo</Modal.Header>
+                <Modal.Content image>
+                  <Image
+                    size="medium"
+                    src="/images/avatar/large/rachel.png"
+                    wrapped
+                  />
+                  <Modal.Description>
+                    <Header>Default Profile Image</Header>
+                    <p>
+                      We've found the following gravatar image associated with
+                      your e-mail address.
+                    </p>
+                    <p>Is it okay to use this photo?</p>
+                  </Modal.Description>
+                </Modal.Content>
+                <Modal.Actions>
+                  <Button
+                    color="black"
+                    //  onClick={}
+                  >
+                    Nope
+                  </Button>
+                  <Button
+                    content="Yep, that's me"
+                    labelPosition="right"
+                    icon="checkmark"
+                    // onClick={() => setOpen(false)}
+                    positive
+                  />
+                </Modal.Actions>
+              </Modal>
+            ) : null}
           </div>
 
           <div ref={forumRef}>
             <Forum
+              setIsFetchSuccessful={setIsFetchSuccessful}
               setIsLoading={setIsLoading}
-              setIsDisplayVisible={setIsDisplayVisible}
-              handleScrollDown={handleScrollDown}
-              setIsRetryingFetch={setIsRetryingFetch}
+              // setIsDisplayVisible={setIsDisplayVisible}
+              // handleScrollDown={handleScrollDown}
+              // setIsRetryingFetch={setIsRetryingFetch}
             />
           </div>
         </div>
--- src/Forum/Forum.jsx
+++ src/Forum/Forum.jsx
@@ -8,7 +8,7 @@ import "./Forum.css";
 function Forum(props) {
   const [forumProfileData, setForumProfileData] = useState(null);
   const [isForumHidden, setIsForumHidden] = useState(true);
-  const [errorOnRetry, setErrorOnRetry] = useState(null);
+  // const [errorOnRetry, setErrorOnRetry] = useState(null);
 
   useEffect(() => {
     getForumProfiles();
@@ -22,71 +22,70 @@ function Forum(props) {
         console.log(response.data);
         setForumProfileData(response.data);
         props.setIsLoading(false);
-        setIsForumHidden(false);
+        props.setIsFetchSuccessful(true);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
         props.setIsLoading(false);
-        setIsForumHidden(false);
+        props.setIsErrorModalDisplayed(true);
+        props.setIsFetchSuccessful(false);
       });
   };
 
-  const handleRetry = () => {
-    props.setIsLoading(true);
-    props.setIsRetryingFetch(true);
-    setIsForumHidden(true);
-    axios
-      .get(apiForumRoot)
-      .then((response) => {
-        console.log(response.data);
-        setForumProfileData(response.data);
-        props.setIsLoading(false);
-        setIsForumHidden(false);
-        props.setIsRetryingFetch(false);
-        // TODO: does not work
-        props.handleScrollDown();
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-        props.setIsLoading(false);
-        setIsForumHidden(false);
-        props.setIsRetryingFetch(false);
-        // TODO: Does not work
-        props.handleScrollDown();
-        setErrorOnRetry(
-          "An error occurred while fetching data. Please try again."
-        );
-      });
-  };
+  // const handleRetry = () => {
+  //   props.setIsLoading(true);
+  //   props.setIsRetryingFetch(true);
+  //   setIsForumHidden(true);
+  //   axios
+  //     .get(apiForumRoot)
+  //     .then((response) => {
+  //       console.log(response.data);
+  //       setForumProfileData(response.data);
+  //       props.setIsLoading(false);
+  //       setIsForumHidden(false);
+  //       props.setIsRetryingFetch(false);
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error fetching data:", error);
+  //       props.setIsLoading(false);
+  //       setIsForumHidden(false);
+  //       props.setIsRetryingFetch(false);
+  //       setErrorOnRetry(
+  //         "An error occurred while fetching data. Please try again."
+  //       );
+  //     });
+  // };
 
   const handleReturn = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
   return (
-    <div className={isForumHidden ? "Hide-Forum" : ""}>
-      {/* Profile cards */}
-      {forumProfileData ? (
-        <div className="Forum-Page">
-          <Grid centered>
-            <Grid.Row style={{ marginTop: "3rem" }}>
-              <Card.Group>
-                {forumProfileData.map((eachForumProfile) => (
-                  <ProfileCard
-                    forumProfile={eachForumProfile}
-                    key={eachForumProfile.forumProfileId}
-                  />
-                ))}
-              </Card.Group>
-            </Grid.Row>
-            <Grid.Row>
-              <Button onClick={handleReturn}> Back to top</Button>
-            </Grid.Row>
-          </Grid>
-        </div>
-      ) : (
-        // Error screen
-        <div>
+    <div>
+      {isForumHidden ? null : (
+        <div className={isForumHidden ? "Hide-Forum" : ""}>
+          {/* Profile cards */}
+          {/* {forumProfileData ? ( */}
+          <div className="Forum-Page">
+            <Grid centered>
+              <Grid.Row style={{ marginTop: "3rem" }}>
+                <Card.Group>
+                  {forumProfileData.map((eachForumProfile) => (
+                    <ProfileCard
+                      forumProfile={eachForumProfile}
+                      key={eachForumProfile.forumProfileId}
+                    />
+                  ))}
+                </Card.Group>
+              </Grid.Row>
+              <Grid.Row>
+                <Button onClick={handleReturn}> Back to top</Button>
+              </Grid.Row>
+            </Grid>
+          </div>
+          {/* ) : ( */}
+          {/* Error screen */}
+          {/* <div>
           <div className="Forum-Error-Page">
             <div>
               There was an issue connecting to the network, please try again
@@ -96,16 +95,17 @@ function Forum(props) {
             </Button>
             <Button style={{ marginTop: "2rem" }} onClick={handleReturn}>
               Return
-            </Button>
-
-            {errorOnRetry ? (
+            </Button> */}
+          {/* {errorOnRetry ? (
               <Message
                 onDismiss={() => setErrorOnRetry(null)}
                 header="Error"
                 content={errorOnRetry}
               />
-            ) : null}
-          </div>
+            ) : null} */}
+          {/* </div> */}
+          {/* </div> */}
+          {/* )} */}
         </div>
       )}
     </div>
