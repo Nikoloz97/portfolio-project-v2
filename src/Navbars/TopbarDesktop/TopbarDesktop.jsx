9f944ad292d45b9fc94b869a973210cc1ae85e96
Nikoloz97 <nick.gotsy@gmail.com>
2023-12-20 00:07:30 -0500

    Improved desktop topbar design

--- src/Navbars/TopbarDesktop/TopbarDesktop.jsx
+++ src/Navbars/TopbarDesktop/TopbarDesktop.jsx
@@ -1,48 +1,109 @@
-import React from "react";
+import React, { useState } from "react";
 import { Menu, MenuItem, Button, Image, Header } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import { useUserContext } from "../../UserContext";
 import "./TopbarDesktop.css";
 
-const TopbarDesktop = (props) => {
+const TopbarDesktop = () => {
   const { user } = useUserContext();
+
+  const [redButtonName, setRedButtonName] = useState("Home");
 
   return (
     <div>
-      <Menu className="Topbar-Desktop">
-        <MenuItem as={Link} to="/" position="right">
-          <Button onClick={props.toggleSidebarVisibility}>Home</Button>
-        </MenuItem>
-        <MenuItem as={Link} to="/blog" position="right">
-          <Button onClick={props.toggleSidebarVisibility}>Blog</Button>
-        </MenuItem>
-        <MenuItem as={Link} to="/projects" position="right">
-          <Button onClick={props.toggleSidebarVisibility}>Projects</Button>
-        </MenuItem>
-        <MenuItem as={Link} to="/forumPage" position="right">
-          <Button onClick={props.toggleSidebarVisibility}>Forum</Button>
-        </MenuItem>
-        <MenuItem as={Link} to="/contact" position="right">
-          <Button onClick={props.toggleSidebarVisibility}>Contact</Button>
-        </MenuItem>
-
-        <Menu.Menu position="right">
-          <MenuItem as={Link} to={user ? "/profilePage" : "/login"}>
-            <Header className="Profile-Message" as="h5">
-              {user !== undefined && user !== null
-                ? `Welcome, ${user.firstName}`
-                : "Log In / Sign Up"}
-            </Header>
-
+      <Menu className="Topbar-Desktop" id="Topbar-Bottom-Border">
+        <Menu.Menu>
+          <MenuItem>
             <Image
-              src={
-                user !== undefined && user !== null && user.profileURL !== null
-                  ? user.profileURL
-                  : "https://react.semantic-ui.com/images/wireframe/square-image.png"
-              }
-              avatar
+              src={require("../../Images/Logo/Borjgali_White.png")}
+              style={{ height: "2rem" }}
             />
           </MenuItem>
+          <MenuItem>
+            <Header style={{ color: "white", marginLeft: "-1rem" }}>
+              Nick's Portfolio
+            </Header>
+          </MenuItem>
+        </Menu.Menu>
+
+        <Menu.Menu style={{ marginLeft: "12%" }}>
+          <MenuItem as={Link} to="/">
+            <Button
+              className={`Topbar-Desktop-Button ${
+                redButtonName === "Home" ? "active" : ""
+              }`}
+              onClick={() => setRedButtonName("Home")}
+            >
+              Home
+            </Button>
+          </MenuItem>
+          <MenuItem as={Link} to="/blog">
+            <Button
+              className={`Topbar-Desktop-Button ${
+                redButtonName === "Blog" ? "active" : ""
+              }`}
+              onClick={() => setRedButtonName("Blog")}
+            >
+              Blog
+            </Button>
+          </MenuItem>
+          <MenuItem as={Link} to="/projects">
+            <Button
+              className={`Topbar-Desktop-Button ${
+                redButtonName === "Projects" ? "active" : ""
+              }`}
+              onClick={() => setRedButtonName("Projects")}
+            >
+              Projects
+            </Button>
+          </MenuItem>
+          <MenuItem as={Link} to="/forumPage">
+            <Button
+              className={`Topbar-Desktop-Button ${
+                redButtonName === "Forum" ? "active" : ""
+              }`}
+              onClick={() => setRedButtonName("Forum")}
+            >
+              Forum
+            </Button>
+          </MenuItem>
+          <MenuItem as={Link} to="/contact">
+            <Button
+              className={`Topbar-Desktop-Button ${
+                redButtonName === "Contact" ? "active" : ""
+              }`}
+              onClick={() => setRedButtonName("Contact")}
+            >
+              Contact
+            </Button>
+          </MenuItem>
+        </Menu.Menu>
+
+        <Menu.Menu position="right">
+          {user == undefined && user == null ? (
+            <div style={{ display: "flex" }}>
+              <MenuItem as={Link} to="/login">
+                <Button
+                  className={`Topbar-Desktop-User-Buttons ${
+                    redButtonName === "Login" ? "active" : ""
+                  }`}
+                  content="Login"
+                  onClick={() => setRedButtonName("Login")}
+                />
+              </MenuItem>
+              <MenuItem as={Link} to="/signup">
+                <Button
+                  className={`Topbar-Desktop-User-Buttons ${
+                    redButtonName === "Signup" ? "active" : ""
+                  }`}
+                  content="Signup"
+                  onClick={() => setRedButtonName("Signup")}
+                />
+              </MenuItem>
+            </div>
+          ) : (
+            <Image src={user.profileURL} avatar />
+          )}
         </Menu.Menu>
       </Menu>
     </div>
