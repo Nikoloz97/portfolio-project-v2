abea6ed6b4ae3dd510dd6c7c7a0781f4199449ca
Nikoloz97 <nick.gotsy@gmail.com>
2024-01-04 23:12:54 -0500

    Improved selection bar for sidebar and topbar; sidebar animates properly

--- src/Navbars/SidebarPhone/SidebarPhone.jsx
+++ src/Navbars/SidebarPhone/SidebarPhone.jsx
@@ -1,54 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Menu, MenuItem, Button } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import "./SidebarPhone.css";
 
 const SidebarPhone = (props) => {
+  const [selectionName, setSelectionName] = useState("Home");
+
+  const handleButtonClick = (buttonName) => {
+    setSelectionName(buttonName);
+
+    setTimeout(() => {
+      props.toggleSidebarVisibility();
+    }, 500);
+  };
+
   return (
-    <div
-    // className="Sidebar-Container"
-    >
+    <div>
       <Menu className="Sidebar-Phone">
-        <MenuItem as={Link} to="/">
+        <MenuItem as={Link} to="/" className={`Sidebar-Button-Container`}>
           <Button
             className="Sidebar-Button"
-            onClick={props.toggleSidebarVisibility}
-          >
-            Home
-          </Button>
+            onClick={() => handleButtonClick("Home")}
+            content="Home"
+          />
         </MenuItem>
-        <MenuItem as={Link} to="/blog">
+        <MenuItem as={Link} to="/blog" className={`Sidebar-Button-Container`}>
           <Button
             className="Sidebar-Button"
-            onClick={props.toggleSidebarVisibility}
-          >
-            Blog
-          </Button>
+            onClick={() => handleButtonClick("Blog")}
+            content="Blog"
+          />
         </MenuItem>
-        <MenuItem as={Link} to="/projects">
+        <MenuItem
+          as={Link}
+          to="/projects"
+          className={`Sidebar-Button-Container`}
+        >
           <Button
             className="Sidebar-Button"
-            onClick={props.toggleSidebarVisibility}
-          >
-            Projects
-          </Button>
+            onClick={() => handleButtonClick("Projects")}
+            content="Projects"
+          ></Button>
         </MenuItem>
-        <MenuItem as={Link} to="/forumPage">
+        <MenuItem
+          as={Link}
+          to="/forumPage"
+          className={`Sidebar-Button-Container`}
+        >
           <Button
             className="Sidebar-Button"
-            onClick={props.toggleSidebarVisibility}
-          >
-            Forum
-          </Button>
+            onClick={() => handleButtonClick("Forum")}
+            content="Forum"
+          />
         </MenuItem>
-        <MenuItem as={Link} to="/contact">
+        <MenuItem
+          as={Link}
+          to="/contact"
+          className={`Sidebar-Button-Container`}
+        >
           <Button
             className="Sidebar-Button"
-            onClick={props.toggleSidebarVisibility}
-          >
-            Contact
-          </Button>
+            onClick={() => handleButtonClick("Contact")}
+            content="Contact"
+          />
         </MenuItem>
+        <div className={`Sidebar-Phone-Selection-Bar ${selectionName}`} />
       </Menu>
     </div>
   );
