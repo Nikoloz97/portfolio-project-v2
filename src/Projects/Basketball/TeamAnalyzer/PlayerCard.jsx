916a823d3838b2413ced9647dc5506fb381c4daa
Nikoloz97 <nick.gotsy@gmail.com>
2024-02-19 23:57:25 -0500

    PlayerCard: Improved UI

--- src/Projects/Basketball/TeamAnalyzer/PlayerCard.jsx
+++ src/Projects/Basketball/TeamAnalyzer/PlayerCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Dropdown, Card, Image, Button, Icon } from "semantic-ui-react";
+import { Dropdown, Card, Image, Button, Icon, Grid } from "semantic-ui-react";
 
 const PlayerCard = (props) => {
   return (
@@ -7,47 +7,47 @@ const PlayerCard = (props) => {
       <div className="Player-Card-Container">
         <Card className="Player-Card">
           <div className="Player-Card-Contents-Container">
-            <div className="Player-Card-Headshot-Container">
-              <Image
-                // src={require("../../../Images/General/avatar-icon.jpg")}
-                src={
-                  props.selectedPlayer.playerURL === ""
-                    ? require("../../../Images/General/avatar-icon.jpg")
-                    : props.selectedPlayer.playerURL
-                }
-                style={{ width: "100%", height: "25%", marginBottom: "80px" }}
-              />
+            <div className="Headshot-Header-Container">
+              <div className="Player-Card-Headshot-Container">
+                <Image
+                  src={
+                    props.selectedPlayer.playerURL === ""
+                      ? require("../../../Images/General/avatar-icon.jpg")
+                      : props.selectedPlayer.playerURL
+                  }
+                  style={{ height: "100%", width: "100%" }}
+                />
+              </div>
+              <div
+                className="Player-Header-Team-Container"
+                style={{ width: "50%" }}
+              >
+                <Card.Header style={{ color: "white" }}>
+                  <h1>{props.selectedPlayer.playerName.toUpperCase()}</h1>
+                  <Card.Meta textAlign="center" style={{ color: "white" }}>
+                    {props.selectedPlayer.teamName}
+                  </Card.Meta>
+                </Card.Header>
+              </div>
             </div>
             <div>
-              <Card.Header style={{ color: "white" }}>
-                <h1>{props.selectedPlayer.playerName.toUpperCase()}</h1>
-              </Card.Header>
-
               <Card.Content>
-                <Card.Meta style={{ color: "white" }}>
-                  {props.selectedPlayer.teamName}
-                </Card.Meta>
-                <Card.Description
-                  style={{
-                    position: "absolute",
-                    top: "50%",
-                    left: "60%",
-                    transform: "translate(-50%, -50%)",
-                  }}
-                >
-                  {props.selectedPlayer.stats.map((stat, index) => (
-                    <div key={index} className="Player-Stat-Card">
-                      <header>{stat.name.toUpperCase()}</header>
-                      <p>{stat.value}</p>
-                    </div>
-                  ))}
+                <Card.Description>
+                  <div className="Player-Stats-Grid">
+                    {props.selectedPlayer.stats.map((stat, index) => (
+                      <div key={index} className="Player-Stat-Card">
+                        <header>{stat.name.toUpperCase()}</header>
+                        <p>{stat.value}</p>
+                      </div>
+                    ))}
+                  </div>
                 </Card.Description>
               </Card.Content>
             </div>
           </div>
         </Card>
 
-        <div className="Player-Card-Buttons-Container">
+        <div className="Player-Card-Buttons-Input-Container">
           <Button
             className="Player-Card-Button"
             disabled={props.playerIndex === 0}
@@ -55,6 +55,13 @@ const PlayerCard = (props) => {
           >
             <Icon name="angle left" />
           </Button>
+          <Dropdown
+            placeholder={props.playerDropdown.placeholder}
+            search
+            selection
+            options={props.playerOptions}
+            onChange={(event) => props.handleDropdownSelection(event)}
+          />
           <Button
             className="Player-Card-Button"
             disabled={props.playerIndex === props.selectedPlayers.length - 1}
@@ -64,17 +71,6 @@ const PlayerCard = (props) => {
           </Button>
         </div>
       </div>
-
-      {/* TODO: Fix dropdown (enter = not working) */}
-      <Dropdown
-        style={{ width: "200px", height: "100%", marginTop: "13px" }}
-        placeholder={props.playerDropdown.placeholder}
-        fluid
-        search
-        selection
-        options={props.playerOptions}
-        onChange={(event) => props.handleDropdownSelection(event)}
-      />
     </div>
   );
 };
