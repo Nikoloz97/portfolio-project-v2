72e73605918177d975eec27f6184b806c5990684
Nikoloz97 <nick.gotsy@gmail.com>
2024-02-25 12:18:04 -0500

    Forum: abstracted mocks into respective files

--- src/__tests__/Forum/ForumPage.test.jsx
+++ src/__tests__/Forum/ForumPage.test.jsx
@@ -2,61 +2,19 @@ import { UserProvider } from "../../UserContext";
 import { render, screen, waitFor, act } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import ForumPage from "../../Forum/ForumPage";
-import axios from "axios";
 
-import { apiForumRoot } from "../../Utils/ApiRoutes";
-import MockAdapter from "axios-mock-adapter";
-
-const mock = new MockAdapter(axios);
+import {
+  getForumData,
+  getFailedServerSideFetch,
+} from "../Mocks/Forum/ForumPageMock";
 
 // Mock the useMediaQuery hook
 jest.spyOn(require("react-responsive"), "useMediaQuery").mockReturnValue(true);
 
 describe("Forum Page", () => {
   describe("Using successful mock fetch", () => {
-    mock.onGet(apiForumRoot).reply(200, [
-      {
-        age: 30,
-        displayName: "Future Hendrix",
-        followerCount: 1,
-        followers: [
-          { displayName: "Drake Parker", followerId: 1, profileURL: "testUrl" },
-        ],
-        followingCount: 1,
-        followings: [
-          { displayName: "Drake Parker", followerId: 1, profileURL: "testUrl" },
-        ],
-        forumProfileId: 1,
-        interests: [
-          { description: "Smokes weed and gets high", interestId: 3 },
-        ],
-        posts: [
-          {
-            commentCount: 1,
-            comments: [
-              {
-                commentDate: "2023-07-12T20:38:15",
-                commentId: 1,
-                displayName: "Tom Segura",
-                flags: 0,
-                likes: 5,
-                profileUrl: "testUrl",
-                text: "First.",
-              },
-            ],
-            flags: 0,
-            likes: 0,
-            photoURL: "testPhotoUrl",
-            postId: 1,
-            postedDate: "2023-07-09T17:38:15",
-            text: "I prefer working with 21 Savage than Drake - I mean look at that smile.",
-            title: "Drake vs 21 Savage",
-          },
-        ],
-        postsCount: 1,
-        profileURL: "testURL",
-      },
-    ]);
+    getForumData();
+
     it("renders Loader element in Display component on render", async () => {
       await act(async () => {
         render(
@@ -102,8 +60,8 @@ describe("Forum Page", () => {
     // });
   });
 
-  describe("Using un-successful mock fetch", () => {
-    mock.onGet(apiForumRoot).reply(500);
+  describe("Using failed mock fetch", () => {
+    getFailedServerSideFetch();
 
     it("renders ForumErrorModal component on unsuccessful fetch", async () => {
       await act(async () => {
---
+++ src/__tests__/Mocks/Forum/ForumMock.jsx
@@ -0,0 +1,80 @@
+export const forumProfileData = [
+  {
+    age: 30,
+    displayName: "Future Hendrix",
+    followerCount: 1,
+    followers: [
+      { displayName: "Drake Parker", followerId: 1, profileURL: "testUrl" },
+    ],
+    followingCount: 1,
+    followings: [
+      { displayName: "Drake Parker", followerId: 1, profileURL: "testUrl" },
+    ],
+    forumProfileId: 1,
+    interests: [{ description: "Smokes weed and gets high", interestId: 3 }],
+    posts: [
+      {
+        commentCount: 1,
+        comments: [
+          {
+            commentDate: "2023-07-12T20:38:15",
+            commentId: 1,
+            displayName: "Tom Segura",
+            flags: 0,
+            likes: 5,
+            profileUrl: "testUrl",
+            text: "First.",
+          },
+        ],
+        flags: 0,
+        likes: 0,
+        photoURL: "testPhotoUrl",
+        postId: 1,
+        postedDate: "2023-07-09T17:38:15",
+        text: "I prefer working with 21 Savage than Drake - I mean look at that smile.",
+        title: "Drake vs 21 Savage",
+      },
+    ],
+    postsCount: 1,
+    profileURL: "testURL",
+  },
+  {
+    age: 30,
+    displayName: "Future Hendrix",
+    followerCount: 1,
+    followers: [
+      { displayName: "Drake Parker", followerId: 1, profileURL: "testUrl" },
+    ],
+    followingCount: 1,
+    followings: [
+      { displayName: "Drake Parker", followerId: 1, profileURL: "testUrl" },
+    ],
+    forumProfileId: 2,
+    interests: [{ description: "Smokes weed and gets high", interestId: 3 }],
+    posts: [
+      {
+        commentCount: 1,
+        comments: [
+          {
+            commentDate: "2023-07-12T20:38:15",
+            commentId: 1,
+            displayName: "Tom Segura",
+            flags: 0,
+            likes: 5,
+            profileUrl: "testUrl",
+            text: "First.",
+          },
+        ],
+        flags: 0,
+        likes: 0,
+        photoURL: "testPhotoUrl",
+        postId: 1,
+        postedDate: "2023-07-09T17:38:15",
+        text: "I prefer working with 21 Savage than Drake - I mean look at that smile.",
+        title: "Drake vs 21 Savage",
+      },
+    ],
+    postsCount: 1,
+    profileURL: "testURL",
+  },
+];
