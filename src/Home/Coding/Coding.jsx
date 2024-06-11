36ffe14c82475732dad8cecf6eaddcbb3ff79024
Nikoloz97 <nick.gotsy@gmail.com>
2024-06-10 20:55:48 -0400

    Coding: improved screen UI

--- src/Home/Coding/Coding.jsx
+++ src/Home/Coding/Coding.jsx
@@ -20,23 +20,56 @@ const Coding = (props) => {
   return (
     <div className={`Coding-Screen ${isDesktop ? "Desktop" : "Phone"}`}>
       <div className="Coding-Content">
-        <div className="Coding-Header">Coding</div>
-        <div className="Coding-Subheader-One">Self-Taught Exploration</div>
-        <div className="Coding-Subheader-Two">Jan - Aug 2022</div>
+        <div className="Coding-Headers-Container">
+          <div className="Coding-Header">Coding</div>
+          <div className="Coding-Subheader-One">Self-Taught Exploration</div>
+          <div className="Coding-Subheader-Two">Jan - Aug 2022</div>
+        </div>
         <div className="Coding-Cards-Container">
           <div className="Coding-Card">
             <img
               src={require("../../Images/Home/Coding/Content/Mosh.png")}
+              style={{ width: "100%" }}
               alt="Mosh Youtube Logo"
             />
-            <div>
+            <div className="Coding-Card-Text">
               I did his courses on HTML, CSS, python, and a little bit of Java
               (until I realized it wasn’t the same thing as JavaScript)
             </div>
           </div>
-          <div className="Coding-Card">Media 2</div>
-          <div className="Coding-Card">Media 3</div>
-          <div className="Coding-Card">Media 4</div>
+          <div className="Coding-Card">
+            <img
+              src={require("../../Images/Home/Coding/Content/Mosh.png")}
+              style={{ width: "100%" }}
+              alt="Python notes scroll-through"
+            />
+            <div className="Coding-Card-Text">
+              I did his courses on HTML, CSS, python, and a little bit of Java
+              (until I realized it wasn’t the same thing as JavaScript)
+            </div>
+          </div>
+          <div className="Coding-Card">
+            <img
+              src={require("../../Images/Home/Coding/Content/Mosh.png")}
+              style={{ width: "100%" }}
+              alt="Python github screenshot"
+            />
+            <div className="Coding-Card-Text">
+              I did his courses on HTML, CSS, python, and a little bit of Java
+              (until I realized it wasn’t the same thing as JavaScript)
+            </div>
+          </div>
+          <div className="Coding-Card">
+            <img
+              src={require("../../Images/Home/Coding/Content/Mosh.png")}
+              style={{ width: "100%" }}
+              alt="Georgia website scroll-through"
+            />
+            <div className="Coding-Card-Text">
+              I did his courses on HTML, CSS, python, and a little bit of Java
+              (until I realized it wasn’t the same thing as JavaScript)
+            </div>
+          </div>
         </div>
       </div>
     </div>
