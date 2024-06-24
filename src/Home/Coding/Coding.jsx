d1f53db9c9b549c02e789086a17ee35d0b577fed
Nikoloz97 <nick.gotsy@gmail.com>
2024-06-23 20:42:33 -0400

    Coding: imported media and updated captions

--- src/Home/Coding/Coding.jsx
+++ src/Home/Coding/Coding.jsx
@@ -11,6 +11,10 @@ import biochemImage from "../../Images/Home/Coding/Content/Bootcamp/BiochemSite1
 import nickJsImage from "../../Images/Home/Coding/Content/Bootcamp/NickJS1.png";
 import restaurantTinderImage from "../../Images/Home/Coding/Content/Bootcamp/RT1.png";
 
+import portalImage from "../../Images/Home/Coding/Content/Work/ClaimGenPortal.png";
+import cookiesImage from "../../Images/Home/Coding/Content/Work/CookiesArticle.png";
+import financeImage from "../../Images/Home/Coding/Content/Work/FinanceWebsite.png";
+
 const Coding = () => {
   const { isDesktop } = useUserContext();
 
@@ -26,7 +30,7 @@ const Coding = () => {
           techStack: null,
           mediaUrl: moshImage,
           mediaAltText: "Mosh logo",
-          mediaLinkUrl: null,
+          websiteLinkUrl: null,
           mediaCaption:
             "I began my coding journey with Mosh's courses on HTML, CSS, Python, and a little bit of Java (until I realized it wasn’t the same thing as JavaScript)",
         },
@@ -35,7 +39,7 @@ const Coding = () => {
           techStack: null,
           mediaUrl: pythonImage,
           mediaAltText: "Google docs Python notes",
-          mediaLinkUrl: null,
+          websiteLinkUrl: null,
           mediaCaption:
             "I took thorough notes. In addition to python above, I made notes to an Intro to Programming video by FreeCodeCamp and HTML/CSS by mosh",
         },
@@ -44,7 +48,7 @@ const Coding = () => {
           techStack: "HTML, CSS",
           mediaUrl: georgiaImage,
           mediaAltText: "Website showing my trip in Georgia from 2021",
-          mediaLinkUrl: "https://nickgeorgiatrip2021.netlify.app/",
+          websiteLinkUrl: "https://nickgeorgiatrip2021.netlify.app/",
           mediaCaption:
             "Using my HTML & CSS notes, I created my first website with the help of bootstrap. It was my first instance of putting my front-end skills to practice. Click on the image above to see the website",
         },
@@ -59,7 +63,7 @@ const Coding = () => {
           techStack: "Vue.js, C#, SQL",
           mediaUrl: restaurantTinderImage,
           mediaAltText: "Github for capstone project",
-          mediaLinkUrl: null,
+          websiteLinkUrl: null,
           mediaCaption:
             "My bootcamp's curriculum involved 3 group coding projects. The last one involved a restaurant match-making system called Restaurant Tinder. It consists of the following tech stack: Vue.js, C#, and SQL",
         },
@@ -68,7 +72,7 @@ const Coding = () => {
           techStack: "JavaScript",
           mediaUrl: nickJsImage,
           mediaAltText: "Intro screen to my portfolio page",
-          mediaLinkUrl: null,
+          websiteLinkUrl: "https://nikoloz97.github.io/",
           mediaCaption:
             "During my downtime, I made a portfolio page using Vanilla JavaScript",
         },
@@ -76,10 +80,10 @@ const Coding = () => {
           header: "First Passion Project",
           techStack: "Vue, C#, SQLite",
           mediaUrl: biochemImage,
-          mediaAltText: "Intr screen to my biochemistry site",
-          mediaLinkUrl: null,
+          mediaAltText: "Intro screen to my biochemistry site",
+          websiteLinkUrl: null,
           mediaCaption:
-            "I worked as an online tutor before my bootcamp. It was there that I saved useful notes into various word documents. My first passion project was building a website where such notes could be shared with my students",
+            "Before I began my bootcamp journey, I worked as an online tutor. A frequent problem I faced was being able to share the useful notes I collected in a convenient way. That is when I started creating a website to do exactly that. ",
         },
       ],
     },
@@ -90,29 +94,30 @@ const Coding = () => {
         {
           header: "ClaimGen Website",
           techStack: "React, ASP.NET, SQL",
-          mediaUrl: "../Images/Home/Coding/Content/Mosh.png",
-          mediaAltText: "",
-          mediaLinkUrl: null,
+          mediaUrl: portalImage,
+          mediaAltText: "One of the screens of the ClaimGen Portal website",
+          websiteLinkUrl: null,
           mediaCaption:
-            "I did his courses on HTML, CSS, python, and a little bit of Java (until I realized it wasn’t the same thing as JavaScript)",
+            "As a full-stack developer at AccumTech, I work on fixing bugs and improving features on our website called ClaimGen Portal. It is a place where Business Analysts, Data Operations, and Health Insurance Vendors work with patient insurance claims",
         },
         {
           header: "Technical Documentation",
           techStack: null,
-          mediaUrl: "../Images/Home/Coding/Content/Mosh.png",
-          mediaAltText: "",
-          mediaLinkUrl: null,
+          mediaUrl: cookiesImage,
+          mediaAltText:
+            "One of the technical documents written regarding implementation of website Cookies",
+          websiteLinkUrl: null,
           mediaCaption:
-            "I really went overboard with the notes. In addition to the one above, I made notes to: Intro to Programming and HTML",
+            "Through every new project, I document my process for other developers to follow accordingly. If novel enough, I conduct presentations in front of my whole developer team",
         },
         {
           header: "Finance Side Project",
           techStack: "TypeScript, React, Express, MongoDb",
-          mediaUrl: "../Images/Home/Coding/Content/Mosh.png",
-          mediaAltText: "",
-          mediaLinkUrl: null,
+          mediaUrl: financeImage,
+          mediaAltText: "Home screen of my finance side project",
+          websiteLinkUrl: null,
           mediaCaption:
-            "The notes also gave me introduction to using a code editor and source control",
+            "Finance Viewer is a side project I have been working on beginning in April 2024. As the name implies, my goal is create a place where users can view all their finances in one location",
         },
       ],
     },
@@ -178,9 +183,9 @@ const Coding = () => {
             </div>
           </div>
           <div className="Coding-Card">
-            {codingPeriods[periodIndex].cards[2].mediaLinkUrl ? (
+            {codingPeriods[periodIndex].cards[2].websiteLinkUrl ? (
               <a
-                href={codingPeriods[periodIndex].cards[2].mediaLinkUrl}
+                href={codingPeriods[periodIndex].cards[2].websiteLinkUrl}
                 target="_blank"
                 rel="noopener noreferrer"
               >
