3c71253d5174bae869c8a8b5fc2ad00bd701b023
Nikoloz97 <nick.gotsy@gmail.com>
2024-10-18 19:26:48 -0400

    Projects: installed mathjs for calculations; improved UI for calculator

--- src/Projects/Calculator/Calculator.jsx
+++ src/Projects/Calculator/Calculator.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./Calculator.css";
 import { Button } from "semantic-ui-react";
+import { evaluate } from "mathjs";
 
 function Calculator() {
   const [result, setResult] = useState("");
@@ -9,19 +10,26 @@ function Calculator() {
     setResult(result.concat(event.target.name));
   };
 
+  const calculateSquareRoot = () => {
+    try {
+      const sqrtResult = evaluate(`sqrt(${result})`);
+      setResult(sqrtResult.toString());
+    } catch (error) {
+      setResult("Error");
+    }
+  };
+
   const clear = () => {
     setResult("");
   };
 
-  // -1 = last item in array = omitted
   const backspace = () => {
     setResult(result.slice(0, -1));
   };
 
   const calculate = () => {
     try {
-      // TODO: Replace eval function (not best practice??)
-      setResult(eval(result).toString());
+      setResult(evaluate(result).toString());
     } catch (error) {
       setResult("Error");
     }
@@ -33,71 +41,108 @@ function Calculator() {
       <div className="Calc-Display-Buttons-Container">
         <div className="Calc-Display">{result}</div>
         <div className="Calc-Buttons-Container">
-          {/* name -> event.target.name */}
-          <div className="Calc-Section-Container">
-            {/* TODO: make sure this button works */}
-            <Button name="%" onClick={handleClick}>
+          <div className="Calc-Circular-Buttons-Container">
+            <Button
+              className="Calc-Circular-Button"
+              name="%"
+              onClick={handleClick}
+            >
               %
             </Button>
-            <Button name="7" onClick={handleClick}>
-              7
+            <Button
+              className="Calc-Circular-Button"
+              name="√"
+              onClick={calculateSquareRoot}
+            >
+              √
             </Button>
-            <Button name="8" onClick={handleClick}>
-              8
+            <Button className="Calc-Circular-Button" onClick={clear}>
+              Clear
             </Button>
-            <Button name="9" onClick={handleClick}>
-              9
+            <Button className="Calc-Circular-Button" onClick={backspace}>
+              C
             </Button>
-            <Button name="/" onClick={handleClick}>
+          </div>
+
+          <div className="Calc-Number-Buttons-Container">
+            <div className="Calc-Number-Buttons-Row">
+              <Button name="7" onClick={handleClick}>
+                7
+              </Button>
+              <Button name="8" onClick={handleClick}>
+                8
+              </Button>
+              <Button name="9" onClick={handleClick}>
+                9
+              </Button>
+            </div>
+            <div className="Calc-Number-Buttons-Row">
+              <Button name="4" onClick={handleClick}>
+                4
+              </Button>
+              <Button name="5" onClick={handleClick}>
+                5
+              </Button>
+              <Button name="6" onClick={handleClick}>
+                6
+              </Button>
+            </div>
+            <div className="Calc-Number-Buttons-Row">
+              <Button name="1" onClick={handleClick}>
+                1
+              </Button>
+              <Button name="2" onClick={handleClick}>
+                2
+              </Button>
+              <Button name="3" onClick={handleClick}>
+                3
+              </Button>
+            </div>
+            <div className="Calc-Number-Buttons-Row">
+              <Button name="0" onClick={handleClick}>
+                0
+              </Button>
+              <Button name="." onClick={handleClick}>
+                .
+              </Button>
+            </div>
+          </div>
+
+          <div className="Calc-Circular-Buttons-Container">
+            <Button
+              className="Calc-Circular-Button"
+              name="/"
+              onClick={handleClick}
+            >
               /
             </Button>
-          </div>
-          <div className="Calc-Section-Container">
-            {/* TODO: make sure this button works */}
-            <Button name="sqr-root" onClick={handleClick}>
-              sqr root
+            <Button
+              className="Calc-Circular-Button"
+              name="*"
+              onClick={handleClick}
+            >
+              *
             </Button>
-            <Button name="4" onClick={handleClick}>
-              4
-            </Button>
-            <Button name="5" onClick={handleClick}>
-              5
-            </Button>
-            <Button name="6" onClick={handleClick}>
-              6
-            </Button>
-            <Button name="*" onClick={handleClick}>
-              x
-            </Button>
-          </div>
-          <div className="Calc-Section-Container">
-            <Button onClick={clear}>Clear</Button>
-            <Button name="1" onClick={handleClick}>
-              1
-            </Button>
-            <Button name="2" onClick={handleClick}>
-              2
-            </Button>
-            <Button name="3" onClick={handleClick}>
-              3
-            </Button>
-            <Button name="-" onClick={handleClick}>
+            <Button
+              className="Calc-Circular-Button"
+              name="-"
+              onClick={handleClick}
+            >
               -
             </Button>
-          </div>
-          <div className="Calc-Section-Container">
-            <Button onClick={backspace}>C</Button>
-            <Button name="0" onClick={handleClick}>
-              0
-            </Button>
-            <Button name="." onClick={handleClick}>
-              .
-            </Button>
-            <Button name="+" onClick={handleClick}>
+            <Button
+              className="Calc-Circular-Button"
+              name="+"
+              onClick={handleClick}
+            >
               +
             </Button>
-            <Button onClick={calculate}>=</Button>
           </div>
+        </div>
+        <div className="Calc-Equals-Button-Container">
+          <Button className="Calc-Equals-Button" onClick={calculate}>
+            =
+          </Button>
         </div>
       </div>
     </div>
