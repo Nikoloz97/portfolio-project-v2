07028683114e8c4eba45d47ead22e71e3d7161a1
Nikoloz97 <nick.gotsy@gmail.com>
2024-10-18 14:56:05 -0400

    Projects: improved calculator UI

--- src/Projects/Calculator/Calculator.jsx
+++ src/Projects/Calculator/Calculator.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./Calculator.css";
+import { Button } from "semantic-ui-react";
 
 function Calculator() {
   const [result, setResult] = useState("");
@@ -19,7 +20,7 @@ function Calculator() {
 
   const calculate = () => {
     try {
-      // TODO: Replace eval function
+      // TODO: Replace eval function (not best practice??)
       setResult(eval(result).toString());
     } catch (error) {
       setResult("Error");
@@ -29,58 +30,75 @@ function Calculator() {
   return (
     <div className="Default-Page">
       <h1>Calculator Project</h1>
-      <div>{result}</div>
-      <div>
-        <button onClick={clear}>Clear</button>
-        <button onClick={backspace}>C</button>
-
-        {/* name -> event.target.name */}
-        <button name="+" onClick={handleClick}>
-          +
-        </button>
-        <button name="7" onClick={handleClick}>
-          7
-        </button>
-        <button name="8" onClick={handleClick}>
-          8
-        </button>
-        <button name="9" onClick={handleClick}>
-          9
-        </button>
-        <button name="-" onClick={handleClick}>
-          -
-        </button>
-        <button name="4" onClick={handleClick}>
-          4
-        </button>
-        <button name="5" onClick={handleClick}>
-          5
-        </button>
-        <button name="6" onClick={handleClick}>
-          6
-        </button>
-        <button name="*" onClick={handleClick}>
-          x
-        </button>
-        <button name="1" onClick={handleClick}>
-          1
-        </button>
-        <button name="2" onClick={handleClick}>
-          2
-        </button>
-        <button name="3" onClick={handleClick}>
-          3
-        </button>
-        <button name="/" onClick={handleClick}>
-          /
-        </button>
-        <button name="0" onClick={handleClick}>
-          0
-        </button>
-        <button name="." onClick={handleClick}>
-          .
-        </button>
-        <button onClick={calculate}>=</button>
+      <div className="Calc-Display-Buttons-Container">
+        <div className="Calc-Display">{result}</div>
+        <div className="Calc-Buttons-Container">
+          {/* name -> event.target.name */}
+          <div className="Calc-Section-Container">
+            {/* TODO: make sure this button works */}
+            <Button name="%" onClick={handleClick}>
+              %
+            </Button>
+            <Button name="7" onClick={handleClick}>
+              7
+            </Button>
+            <Button name="8" onClick={handleClick}>
+              8
+            </Button>
+            <Button name="9" onClick={handleClick}>
+              9
+            </Button>
+            <Button name="/" onClick={handleClick}>
+              /
+            </Button>
+          </div>
+          <div className="Calc-Section-Container">
+            {/* TODO: make sure this button works */}
+            <Button name="sqr-root" onClick={handleClick}>
+              sqr root
+            </Button>
+            <Button name="4" onClick={handleClick}>
+              4
+            </Button>
+            <Button name="5" onClick={handleClick}>
+              5
+            </Button>
+            <Button name="6" onClick={handleClick}>
+              6
+            </Button>
+            <Button name="*" onClick={handleClick}>
+              x
+            </Button>
+          </div>
+          <div className="Calc-Section-Container">
+            <Button onClick={clear}>Clear</Button>
+            <Button name="1" onClick={handleClick}>
+              1
+            </Button>
+            <Button name="2" onClick={handleClick}>
+              2
+            </Button>
+            <Button name="3" onClick={handleClick}>
+              3
+            </Button>
+            <Button name="-" onClick={handleClick}>
+              -
+            </Button>
+          </div>
+          <div className="Calc-Section-Container">
+            <Button onClick={backspace}>C</Button>
+            <Button name="0" onClick={handleClick}>
+              0
+            </Button>
+            <Button name="." onClick={handleClick}>
+              .
+            </Button>
+            <Button name="+" onClick={handleClick}>
+              +
+            </Button>
+            <Button onClick={calculate}>=</Button>
+          </div>
+        </div>
       </div>
     </div>
   );
