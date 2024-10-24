01a5dca15c691976e1ed21211988f995b86129f2
Nikoloz97 <nick.gotsy@gmail.com>
2024-10-23 21:19:17 -0400

    Routes are now pointed towards azure endpoints

--- src/Utils/ApiRoutes.js
+++ src/Utils/ApiRoutes.js
@@ -1,18 +1,20 @@
-export const apiUserRoot = "https://localhost:7047/api/user";
+// TODO: delete these?
 
-export const apiForumRoot = "https://localhost:7047/api/forum";
+// export const apiUserRoot = "https://localhost:7047/api/user";
 
-export const apiGeoGameRoot = "https://localhost:7047/api/geoGame";
+// export const apiForumRoot = "https://localhost:7047/api/forum";
 
-export const apiContactRoot = "https://localhost:7047/api/contact";
+// export const apiGeoGameRoot = "https://localhost:7047/api/geoGame";
 
-// export const apiUserRoot = "https://nicksportfolio.azurewebsites.net/api/user";
+// export const apiContactRoot = "https://localhost:7047/api/contact";
 
-// export const apiForumRoot =
-//   "https://nicksportfolio.azurewebsites.net/api/forum";
+export const apiUserRoot = "https://nicksportfolio.azurewebsites.net/api/user";
 
-// export const apiGeoGameRoot =
-//   "https://nicksportfolio.azurewebsites.net/api/geoGame";
+export const apiForumRoot =
+  "https://nicksportfolio.azurewebsites.net/api/forum";
 
-// export const apiContactRoot =
-//   "https://nicksportfolio.azurewebsites.net/api/contact";
+export const apiGeoGameRoot =
+  "https://nicksportfolio.azurewebsites.net/api/geoGame";
+
+export const apiContactRoot =
+  "https://nicksportfolio.azurewebsites.net/api/contact";
