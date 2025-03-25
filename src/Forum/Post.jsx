4363479bd717741bc85c984e2cb7750a07e92ca0
Nikoloz97 <nick.gotsy@gmail.com>
2025-03-24 23:40:28 -0400

    Improved forum page UI

--- src/Forum/Post.jsx
+++ src/Forum/Post.jsx
@@ -4,14 +4,14 @@ import "./Forum.css";
 const Post = (props) => {
   return (
     <div className="Post-Title-Text-Image-Container">
-      <div>
+      <div className="Post-Title-Text-Container">
+        {/* <h1 style={{ textAlign: "center" }}>{props.post.title}</h1> */}
+        <div className="Post-Description-Container">{props.post.text}</div>
+      </div>
+      <div className="Post-Image-Container">
         {props.post.photoURL && (
           <img className="Post-Image" src={props.post.photoURL} alt="post" />
         )}
-      </div>
-      <div className="Post-Title-Text-Container">
-        <h1 style={{ textAlign: "center" }}>{props.post.title}</h1>
-        <div className="Post-Description-Container">{props.post.text}</div>
       </div>
     </div>
   );
--- src/Forum/ProfileCard.jsx
+++ src/Forum/ProfileCard.jsx
@@ -10,39 +10,32 @@ const ProfileCard = (props) => {
 
   return (
     <div className="Forum-Card">
-      <Post post={currentPost} />
-      <div className="ReadMore-Container">
-        <div className="Read-More-Button-Container">
-          <Button color="black" className="Read-More-Button">
-            Read Full Post
-          </Button>
-        </div>
-      </div>
       <div className="Forum-Card-ProfileImage-Name-Date-ReadMore-Container">
-        <div className="Forum-Card-ProfileImage-Container">
-          {props.forumProfile.profileURL ? (
-            <img
-              className="Forum-Card-Image"
-              src={props.forumProfile.profileURL}
-              alt="profile card"
-            />
-          ) : (
-            <div className="Forum-Card-Image">
-              <Icon
-                name="user circle"
-                size="big"
-                style={{ marginTop: "0.1em" }}
+        <div className="Forum-Card-ProfileImage-Name-Container">
+          <div className="Forum-Card-ProfileImage-Container">
+            {props.forumProfile.profileURL ? (
+              <img
+                className="Forum-Card-Image"
+                src={props.forumProfile.profileURL}
+                alt="profile card"
               />
-            </div>
-          )}
-        </div>
-        <div className="Forum-Card-Name-Date-Container">
+            ) : (
+              <div className="Forum-Card-Image">
+                <Icon
+                  name="user circle"
+                  size="huge"
+                  style={{ marginTop: "0.1em" }}
+                />
+              </div>
+            )}
+          </div>
           <p style={{ marginBottom: "0" }}>{props.forumProfile.displayName}</p>
-          <p style={{ marginTop: "0" }}>
-            {postDateFormatter(currentPost.postedDate.toLocaleString())}
-          </p>
         </div>
+        <p style={{ marginTop: "0" }}>
+          {postDateFormatter(currentPost.postedDate.toLocaleString())}
+        </p>
       </div>
+      <Post post={currentPost} />
     </div>
   );
 };
